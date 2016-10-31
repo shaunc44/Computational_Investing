3a653ff5513b62af1a8cb0d85cf56a1e2739bc4c
Shaun <seyyes2000@hotmail.com>
2016-10-31 17:18:04 -0400

    Begin portolio optimization program

---
+++ hw_1_optimize_portfolio.py
@@ -0,0 +1,8 @@
+import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkutil.DataAccess as da
+
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas as pd
+
