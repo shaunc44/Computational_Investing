bb1a2c287212ebb87d07d0d3c7a5a9ef6453231b
Shaun <seyyes2000@hotmail.com>
2016-11-01 06:05:36 -0400

    Continue portolio optimization program

--- hw_1_optimize_portfolio.py
+++ hw_1_optimize_portfolio.py
@@ -6,3 +6,34 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import pandas as pd
 
+ls_symbols = ["GOOG", "AAPL", "GLD", "XOM"]
+allocations = [0.4, 0.4, 0.0, 0.2] #should this be a list, and here?
+dt_start = dt.datetime(2011, 1, 1)
+dt_end = dt.datetime(2011, 12, 31)
+dt_timeofday = dt.timedelta(hours=16)
+ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
+
+#clear cache on this line??
+c_dataobj = da.DataAccess('Yahoo')
+ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+d_data = dict(zip(ls_keys, ldf_data))
+
+
+
+
+'''
+def simulation():
+	#put in list??
+	adj_closing_prices = []
+	adj_closing_prices = ls_keys[3]
+	print adj_closing_prices
+	#normalized_price
+
+	return st_dev
+	return daily_ret_portfolio
+	return sharpe_ratio
+	return total_ret_portfolio
+
+simulation()
+'''
