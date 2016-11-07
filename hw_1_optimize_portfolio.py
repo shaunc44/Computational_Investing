6e37de812b0e5e9d1765a2f6a337cf3820c810b8
Shaun <seyyes2000@hotmail.com>
2016-11-07 17:30:13 -0500

    Continue portolio optimization program hw1

--- hw_1_optimize_portfolio.py
+++ hw_1_optimize_portfolio.py
@@ -5,15 +5,30 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import math
 
 
+#calculate portfolio daily rets everyday and put in list??
 def avg_daily_return():
 	avg_daily_rets = 0
 	for i in range(len(ls_symbols)):
-		avg_daily_rets += (np.mean(daily_returns[:,i], dtype=np.float64) * allocations[i])
-		#print allocations[i]
+		avg_daily_rets += (np.mean(daily_returns[:,i]) * allocations[i])
 		#print avg_daily_rets
 	return avg_daily_rets
+
+
+def volatility():
+	stdev_daily_rets = 0
+	for i in range(len(ls_symbols)):
+		stdev_daily_rets = (np.std(daily_returns[:,i]) * allocations[i])
+		#print stdev_daily_rets
+	return stdev_daily_rets
+
+
+def sharpe_ratio():
+	k = math.sqrt(250)
+	sharpe = k * (avg_daily_return()/volatility())
+	return sharpe
 
 
 def simulate(*args):
@@ -21,14 +36,14 @@ def simulate(*args):
 	print "End Date: ", dt_end.strftime("%B %d, %Y")
 	print "Symbols: ", ls_symbols
 	print "Optimal Allocations: ", allocations #how to insert optimal allocations?
-	print "Sharpe Ratio: "
-	print "Volatility (stdev of daily rets): ", stdev_daily_rets
 	print "Avg Daily Return: ", avg_daily_return()
+	print "Volatility (stdev of daily rets): ", volatility()
+	print "Sharpe Ratio: ", sharpe_ratio()
 	print "Cumulative Return: "
 
 
 ls_symbols = ['AAPL', 'GLD', 'GOOG', 'XOM']
-allocations = [1.0, 0.0, 0.0, 0.0]
+allocations = [0.4, 0.4, 0.0, 0.2]
 dt_start = dt.datetime(2011, 1, 1)
 dt_end = dt.datetime(2011, 12, 31)
 dt_timeofday = dt.timedelta(hours=16)
@@ -51,53 +66,8 @@ na_normalized_price = na_price / na_price[0,:]
 #make copy of normalized_price instead of reference
 na_rets = na_normalized_price.copy()
 daily_returns = tsu.returnize0(na_rets)
-#print daily_returns
+print len(daily_returns)
 
-#total returns and average return is not correct for Apple
-print np.sum(daily_returns[:,0])
-
-'''
-#Figure out how to apply weights to for loop results below
-weighted_ret = 0
-for i in range(len(allocations)):
-	weighted_ret += np.mean(daily_returns[:,i]) * allocations[i]
-	#print np.std(daily_returns[:,i])
-print weighted_ret
-'''
-#print len(allocations)
-
-
-
-'''
-annual_ret_stock1 = np.mean(daily_returns[:,0]) * len(daily_returns)
-annual_ret_stock2 = np.mean(daily_returns[:,1]) * len(daily_returns)
-annual_ret_stock3 = np.mean(daily_returns[:,2]) * len(daily_returns)
-annual_ret_stock4 = np.mean(daily_returns[:,3]) * len(daily_returns)
-print annual_ret_stock1
-print annual_ret_stock2
-print annual_ret_stock3
-print annual_ret_stock4
-'''
-
-
-
-'''
-print annual_ret_stock1 * allocations[0] + annual_ret_stock2 * allocations[1] + annual_ret_stock3 * allocations[2] + annual_ret_stock4 * allocations[3]
-'''
-
-
-'''
-print annual_ret_stock1 * allocations[0]
-print annual_ret_stock2 * allocations[1]
-print annual_ret_stock3 * allocations[2]
-print annual_ret_stock4 * allocations[3]
-'''
-
-
-
-
-stdev_daily_rets = np.std(daily_returns)
-#print stdev_daily_rets
 
 '''
 plt.clf() #Clear the plot
