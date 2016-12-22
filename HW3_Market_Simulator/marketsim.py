cd4ad500681a5126bcf824093d1cd28ef7a504a0
Shaun <seyyes2000@hotmail.com>
2016-12-22 13:29:22 -0500

    create lists for dates and symbols

--- HW3_Market_Simulator/marketsim.py
+++ HW3_Market_Simulator/marketsim.py
@@ -2,10 +2,19 @@ import csv
 
 
 
+dates = []
+symbols = []
+
 
 orders_reader = csv.reader(open('orders.csv', 'r'), delimiter=',')
 for row in orders_reader:
-	print row
+	#print row
+	dates.append(row[0:3])
+	symbols.append(row[3])
+
+print dates
+print symbols
+
 
 
 
