c9ea5957e2cc627172033c558315d3cb05c1498b
Shaun <seyyes2000@hotmail.com>
2016-12-23 15:34:43 -0500

    add remove duplicates code

--- HW3_Market_Simulator/marketsim.py
+++ HW3_Market_Simulator/marketsim.py
@@ -5,17 +5,18 @@ import csv
 dates = []
 symbols = []
 
-
 orders_reader = csv.reader(open('orders.csv', 'r'), delimiter=',')
 for row in orders_reader:
 	#print row
+	#dates.append(''.join(row[0:3]))
 	dates.append(row[0:3])
 	symbols.append(row[3])
 
-dates = list(set(dates))
-symbols = list(set(symbols))
+#dates = set(dates)
+#symbols = list(set(symbols))
 
-
+#print tuple(dates)
+#print set(dates)
 print dates
 print symbols
 
