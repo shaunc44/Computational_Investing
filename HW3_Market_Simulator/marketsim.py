16d77853b878a8e12da0fc6d3f3690c2f7568480
Shaun <seyyes2000@hotmail.com>
2016-12-27 13:39:18 -0500

    finish removing duplicates

--- HW3_Market_Simulator/marketsim.py
+++ HW3_Market_Simulator/marketsim.py
@@ -1,42 +1,24 @@
 import csv
 
 
-
-dates = []
-symbols = []
-orders_no_dupl = []
-
 orders_reader = csv.reader(open('orders.csv', 'rU'), delimiter=',')
-#print orders_reader[1]
+
+#Create orders list
+orders_list = []
 for row in orders_reader:
-	#print row
-	#dates.append(''.join(row[0:3]))
-	#add if stmt to check is row = other rows???
-	orders_no_dupl.append(row)
+	orders_list.append(row)
 
-	#dates.append(row[0:3])
-	#symbols.append(row[3])
+#Remove duplicates from orders list
+orders_unique = []
+for sublist in orders_list:
+	if sublist not in orders_unique:
+		orders_unique.append(sublist)
 
-#Remove duplicates from lists (why??)
-#orders_no_dupl = list(set(orders_no_dupl))
-#dates = set(dates)
-#symbols = list(set(symbols))
-b = list()
-for sublist in orders_no_dupl:
-	if sublist not in orders_no_dupl:
-		b.append(sublist)
+print orders_unique[2][2]
 
-print b
-print orders_no_dupl
+#Create symbols list?
 
-
-'''
-print dates
-print symbols
-'''
-
-
-
+#Create date list?
 
 
 #marketsim(cash, orders_file)
