0071b88af8b2eb8716669e85bab6c6b47549caee
Shaun <seyyes2000@hotmail.com>
2016-12-27 06:02:17 -0500

    work on marketsim.py

--- HW3_Market_Simulator/marketsim.py
+++ HW3_Market_Simulator/marketsim.py
@@ -6,7 +6,7 @@ dates = []
 symbols = []
 orders_no_dupl = []
 
-orders_reader = csv.reader(open('orders.csv', 'r'), delimiter=',')
+orders_reader = csv.reader(open('orders.csv', 'rU'), delimiter=',')
 #print orders_reader[1]
 for row in orders_reader:
 	#print row
@@ -14,14 +14,19 @@ for row in orders_reader:
 	#add if stmt to check is row = other rows???
 	orders_no_dupl.append(row)
 
-	dates.append(row[0:3])
-	symbols.append(row[3])
+	#dates.append(row[0:3])
+	#symbols.append(row[3])
 
 #Remove duplicates from lists (why??)
-orders_no_dupl = list(set(orders_no_dupl))
+#orders_no_dupl = list(set(orders_no_dupl))
 #dates = set(dates)
 #symbols = list(set(symbols))
+b = list()
+for sublist in orders_no_dupl:
+	if sublist not in orders_no_dupl:
+		b.append(sublist)
 
+print b
 print orders_no_dupl
 
 
