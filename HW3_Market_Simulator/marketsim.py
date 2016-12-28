bdcc7460c75c76692b88ab660e64634cb6d678e7
Shaun <seyyes2000@hotmail.com>
2016-12-28 13:00:50 -0500

    updates

--- HW3_Market_Simulator/marketsim.py
+++ HW3_Market_Simulator/marketsim.py
@@ -1,7 +1,10 @@
 import csv
+import numpy as np
 
 
 trades = csv.reader(open('orders.csv', 'rU'), delimiter=',')
+
+#Create a NUMPY array for the dates list? orders list?
 
 #Create orders list
 orders_list = []
@@ -22,8 +25,24 @@ for row in orders_unique:
 	date_list.append(row[0:3])
 
 #print symbol_list
-#print date_list
+print date_list
+
+#Put first and last date in a variable
+begin_date = min(date_list)
+print begin_date
+
+end_date = min(date_list)
+print end_date
+
+
+#use NYSE dates function to create array with right number of elements for each date used in test
+#Use adjusted close
+#Read data into PANDAS dataframe and create daily prices array with symbols as columns and dates as rows
+#Iterate over orders, check prices, update cash($ not invested)
+
 
 
 
 #marketsim(cash, orders_file)
+
+
