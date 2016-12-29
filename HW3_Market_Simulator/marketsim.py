8dea3524518e0f2fc2bb50b49ee5eb31b6988e74
Shaun <seyyes2000@hotmail.com>
2016-12-29 11:48:41 -0500

    convert dates to ints; store begin and end dates

--- HW3_Market_Simulator/marketsim.py
+++ HW3_Market_Simulator/marketsim.py
@@ -6,16 +6,19 @@ trades = csv.reader(open('orders.csv', 'rU'), delimiter=',')
 
 #Create a NUMPY array for the dates list? orders list?
 
+
 #Create orders list
 orders_list = []
 for row in trades:
 	orders_list.append(row)
+
 
 #Remove duplicates from orders list
 orders_unique = []
 for sublist in orders_list:
 	if sublist not in orders_unique:
 		orders_unique.append(sublist)
+
 
 #Create symbols & dates list
 symbol_list = []
@@ -24,30 +27,18 @@ for row in orders_unique:
 	symbol_list.append(row[3])
 	date_list.append(row[0:3])
 
-#print symbol_list
-#print date_list
 
+#Convert date list to list of ints
 date_list2 = []
 for i in date_list:
-	print i
-	#date_list2.append(map(int(i)))
-
-#date_list = map(int, date_list)
-#date_list = [int(i) for i in date_list]
-print date_list2
+	#print map(int, i)
+	date_list2.append(map(int, i))
 
 
 #Put first and last date in a variable
-begin_date = min(date_list)
-print begin_date
+begin_date = min(date_list2)
+end_date = max(date_list2)
 
-end_date = max(date_list)
-print end_date
-
-if date_list[13] > date_list[12]:
-	print date_list[13]
-else:
-	print date_list[12]
 
 
 #use NYSE dates function to create array with right number of elements for each date used in test
@@ -59,5 +50,4 @@ else:
 
 
 #marketsim(cash, orders_file)
-
 
