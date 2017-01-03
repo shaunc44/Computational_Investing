32cc36948288b4856f555ef5bae0ef5a4b01dbfc
Shaun <seyyes2000@hotmail.com>
2017-01-03 13:15:35 -0500

    add datetime code, covrt dates to ints

--- HW3_Market_Simulator/marketsim.py
+++ HW3_Market_Simulator/marketsim.py
@@ -10,7 +10,6 @@ trades = csv.reader(open('orders.csv', 'rU'), delimiter=',')
 
 #Create a NUMPY array for the dates list? orders list?
 
-
 #Create orders list
 orders_list = []
 for row in trades:
@@ -40,21 +39,24 @@ for i in date_list:
 
 
 #Put first and last date in a variable
+#Build NUMPY ARRAY for these dates??????????????
 begin_date = min(date_list2)
+#print begin_date
+#begin_date = begin_date - 1
 end_date = max(date_list2)
-
-print begin_date
-print end_date
 
 
 #use NYSE dates function to create array with right number of elements for each date used in test
 #Use only one nyse timestamp below *******
-ls_symbols = ['AAPL', 'GLD', 'GOOG', 'XOM']
-dt_start = dt.datetime(2011, 1, 1) #add plus 1 here?
-dt_end = dt.datetime(2011, 12, 31) #and here to?
-dt_timeofday = dt.timedelta(hours=16)
-ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
+ls_symbols = ['AAPL', 'GLD', 'GOOG', 'XOM'] # = symbol_list
+dt_start = dt.datetime( begin_date[0], begin_date[1], begin_date[2] - 1 ) #add plus 1 here?, = begin_date
+dt_end = dt.datetime( end_date[0], end_date[1], end_date[2] + 1 ) #and here too?
 ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
+
+print symbol_list
+#print ls_symbols
+print dt_start
+print dt_end
 
 
 
